8dfa16ba6c41583e98ccd065b18cd5c72b9c2286
Anton Atanasov <anton.atanasov86@gmail.com>
2015-05-24 19:47:48 +0300

    Drawing HangMan and bug fixed

--- WorkingGame.cs
+++ WorkingGame.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.DesignerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Main___
+namespace WorkingGame
 {
-    class Main_
+    class WorkingGame
     {
         static void Main()
         {
 
-            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture; 
-            Console.InputEncoding = Encoding.Unicode; 
+            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+            Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
             string doYouWantToPlayAgain;
 
@@ -45,15 +44,15 @@ namespace Main___
                         Console.WriteLine("Enter number from 1 to 8!!!");
                     }
 
-                } while (category != 1 && category != 2 && category != 3 && category != 4 && 
+                } while (category != 1 && category != 2 && category != 3 && category != 4 &&
                                 category != 5 && category != 6 && category != 7 && category != 8);
-            
-            
-            
+
+
+
                 string theWord = RandomWordGen(category - 1); // random word for hangman 
                 GamePlay(theWord);                            // guess the word 
-               
-                
+
+
                 Console.WriteLine("Do you want to play again? Y/N");
                 do
                 {
@@ -73,7 +72,7 @@ namespace Main___
         public static int StartMenu()
         {
             Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "БЕСЕНИЦА"));
-            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}","created by Team GILEAD"));
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "created by Team GILEAD"));
             Console.WriteLine("За игра въведете 1");
             Console.WriteLine("За да въведете нова дума въведете 0");
             int startInput;
@@ -159,6 +158,8 @@ namespace Main___
             Console.WriteLine("6. Растения");
             Console.WriteLine("7. Автомобили (марки/модели)");
             Console.WriteLine("8. Лектори в СофтУни");
+            Console.WriteLine();
+            Console.Write("Моля въдедете число от 1 до 8: ");
             int AddToCategory = int.Parse(Console.ReadLine()); //category input
             Console.Clear();
             string text = Console.ReadLine();
@@ -168,7 +169,7 @@ namespace Main___
                 case 1: string contents = File.ReadAllText(@"Words\Countries.txt");
                     if (contents.Contains(text))
                     {
-                        Console.WriteLine("Duplicate found!");
+                        Console.WriteLine("Тази държава вече съществува!");
                     }
                     else
                     {
@@ -182,7 +183,7 @@ namespace Main___
                 case 2: contents = File.ReadAllText(@"Words\City.txt");
                     if (contents.Contains(text))
                     {
-                        Console.WriteLine("Duplicate found!");
+                        Console.WriteLine("Този град вече съществува!");
                     }
                     else
                     {
@@ -196,7 +197,7 @@ namespace Main___
                 case 3: contents = File.ReadAllText(@"Words\Rivers.txt");
                     if (contents.Contains(text))
                     {
-                        Console.WriteLine("Duplicate found!");
+                        Console.WriteLine("Тази река вече съществува!");
                     }
                     else
                     {
@@ -210,7 +211,7 @@ namespace Main___
                 case 4: contents = File.ReadAllText(@"Words\Mountains.txt");
                     if (contents.Contains(text))
                     {
-                        Console.WriteLine("Duplicate found!");
+                        Console.WriteLine("Тази планина вече съществува!");
                     }
                     else
                     {
@@ -226,7 +227,7 @@ namespace Main___
                 case 5: contents = File.ReadAllText(@"Words\Animals.txt");
                     if (contents.Contains(text))
                     {
-                        Console.WriteLine("Duplicate found!");
+                        Console.WriteLine("Това животно вече съществува!");
                     }
                     else
                     {
@@ -242,7 +243,7 @@ namespace Main___
                 case 6: contents = File.ReadAllText(@"Words\Plants.txt");
                     if (contents.Contains(text))
                     {
-                        Console.WriteLine("Duplicate found!");
+                        Console.WriteLine("Това растение вече съществува!");
                     }
                     else
                     {
@@ -323,6 +324,7 @@ namespace Main___
                 bool guesLetter = false;
                 int howManyLettertAreGuested = 0;
                 Console.WriteLine("Въведохте буквите: {0}", string.Join(", ", enterdLetters));
+                Console.SetCursorPosition(0, 3);
                 for (int i = 0; i < theWordInArrey.Length; i++)
                 {
                     if (letter == theWordInArrey[i])
@@ -339,10 +341,10 @@ namespace Main___
                 {
                     counterForErrors++;
                 }
-                Console.WriteLine("Остават Ви {0} грешни опита! \n\r", 8-counterForErrors);
+                Console.WriteLine("Остават Ви {0} грешни опита! \n\r", 9 - counterForErrors);
                 Console.WriteLine(string.Join(" ", hidenWordArrey));
                 Console.WriteLine();
-                //DrawingGallowsAndHangman(counterForErrors); !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                DrawingGallowAndHangman(counterForErrors);
                 if (counterForErrors == 9)
                 {
                     Console.WriteLine("GAME OVER!!!");
@@ -354,7 +356,7 @@ namespace Main___
                     PlayMusic();
                     break;
                 }
-                Console.WriteLine("Въведете буква:");                
+                Console.WriteLine("Въведете буква:");
                 letter = Char.Parse(Console.ReadLine());
                 enterdLetters.Add(letter.ToString());
             }
@@ -363,14 +365,237 @@ namespace Main___
 
         public static void PlayMusic()
         {
-            Console.Beep(658, 125); Console.Beep(1320, 500); Console.Beep(990, 250); 
+            Console.Beep(658, 125); Console.Beep(1320, 500); Console.Beep(990, 250);
             Console.Beep(1056, 250); Console.Beep(1188, 250); Console.Beep(1320, 125);
             Console.Beep(1188, 125); Console.Beep(1056, 250); Console.Beep(990, 250);
-            Console.Beep(880, 500); Console.Beep(880, 250); Console.Beep(1056, 250); 
-            Console.Beep(1320, 500); Console.Beep(1188, 250); Console.Beep(1056, 250); 
-            Console.Beep(990, 750); Console.Beep(1056, 250); Console.Beep(1188, 500); 
-            Console.Beep(1320, 500); Console.Beep(1056, 500); Console.Beep(880, 500); 
-            Console.Beep(880, 500);           
+            Console.Beep(880, 500); Console.Beep(880, 250); Console.Beep(1056, 250);
+            Console.Beep(1320, 500); Console.Beep(1188, 250); Console.Beep(1056, 250);
+            Console.Beep(990, 750); Console.Beep(1056, 250); Console.Beep(1188, 500);
+            Console.Beep(1320, 500); Console.Beep(1056, 500); Console.Beep(880, 500);
+            Console.Beep(880, 500);
+        }
+
+        public static void DrawFrame() //drawning frame
+        {
+            int frameWidth = 36;
+            int frameHeight = 27;
+            Console.SetCursorPosition(50, 2);
+            Console.Write(new string('-', frameWidth + 2));
+            for (int i = 3; i < frameHeight; i++)
+            {
+                Console.SetCursorPosition(50, i);
+                Console.WriteLine(new string('|', 1) + new string(' ', frameWidth) + new string('|', 1));
+            }
+            Console.SetCursorPosition(50, 27);
+            Console.Write(new string('-', frameWidth + 2));
+        }
+        static int baseGallowWidth = 11;
+        static int ropeHeight = 8;
+        static int gallowTopBaseWidth = 15;
+
+        public static void DrawingGallowBase()
+        {
+            Console.SetBufferSize(90, 31);
+            Console.SetCursorPosition(53, 25);
+            //draw base gallow
+            Console.Write(new string('_', baseGallowWidth));
+            for (int i = 25; i > 4; i--)
+            {
+                Console.SetCursorPosition(58, i);
+                Console.Write("|");
+            }
+        }
+
+        public static void DrawingTopBaseAndRope()
+        {
+            //draw Top Base and rope
+            Console.SetCursorPosition(58, 4);
+            Console.WriteLine(new string('-', gallowTopBaseWidth));
+            //draw rope
+            for (int row = 5; row < ropeHeight; row++)
+            {
+                Console.SetCursorPosition(72, row);
+                Console.WriteLine('|');
+            }
+        }
+
+        static int legsHeight = 4;
+        static int startingColLeg = 71;
+        static int armHeight = 4;
+        static int startingColArm = 71;
+        static int bodyHeight = 7;
+        public static void DrawingHead()
+        {
+            Console.SetCursorPosition(70, 9);
+            Console.WriteLine("-----");
+            Console.SetCursorPosition(69, 10);
+            Console.WriteLine("( - - )");
+            Console.SetCursorPosition(69, 11);
+            Console.WriteLine("|  -  |");
+            Console.SetCursorPosition(70, 12);
+            Console.WriteLine("-----");
+        }
+
+        public static void DrawingBody(int startRow)
+        {
+            for (int i = startRow; i < bodyHeight + startRow; i++)
+            {
+                Console.SetCursorPosition(72, i);
+                Console.WriteLine("|");
+            }
+        }
+
+        public static void DrawingLeftLeg(int startRow)
+        {
+            startingColLeg = 71;
+            for (int i = startRow; i < legsHeight + startRow; i++)
+            {
+                Console.SetCursorPosition(startingColLeg, i);
+                Console.WriteLine("/");
+                startingColLeg--;
+            }
+        }
+
+        public static void DrawingRightLeg(int startRow)
+        {
+            startingColLeg = 73;
+            for (int i = startRow; i < legsHeight + startRow; i++)
+            {
+                Console.SetCursorPosition(startingColLeg, i);
+                Console.WriteLine(@"\");
+                startingColLeg++;
+            }
+        }
+
+        public static void DrawingLeftArm(int startRow)
+        {
+            startingColArm = 71;
+            for (int i = startRow; i > startRow - armHeight; i--)
+            {
+                Console.SetCursorPosition(startingColArm, i);
+                Console.WriteLine(@"\");
+                startingColArm--;
+            }
+        }
+
+        public static void DrawingRightArmAndMessage(int starRow)
+        {
+            startingColArm = 73;
+            for (int i = starRow; i > starRow - armHeight; i--)
+            {
+                Console.SetCursorPosition(startingColArm, i);
+                Console.WriteLine("/");
+                startingColArm++;
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.SetCursorPosition(77, 7);
+            Console.WriteLine("Please,");
+            Console.SetCursorPosition(77, 8);
+            Console.WriteLine("don`t make");
+            Console.SetCursorPosition(77, 9);
+            Console.WriteLine("mistake");
+            Console.SetCursorPosition(77, 10);
+            Console.WriteLine("again!!!");
+        }
+
+        public static void HangMan()
+        {
+            DrawingHead();
+            Console.SetCursorPosition(70,13);
+            Console.WriteLine("         ");
+            Console.SetCursorPosition(70, 14);
+            Console.WriteLine("         ");
+            DrawingBody(15);
+            DrawingLeftLeg(22);
+            DrawingRightLeg(22);
+            DrawingLeftArm(18);
+            DrawingRightArmAndMessage(18);
+
+        }
+        public static void DrawingGallowAndHangman(int countFalsePrediction)
+        {
+            DrawFrame();
+            switch (countFalsePrediction)
+            {
+                case 1:
+                {
+                    DrawingGallowBase();
+                    break;
+                }
+                case 2:
+                {
+                    DrawingGallowBase();
+                    DrawingTopBaseAndRope();
+                    break;
+                }
+                case 3:
+                {
+                    DrawingGallowBase();
+                    DrawingTopBaseAndRope();
+                    DrawingHead();
+                    break;
+                }
+                case 4:
+                {
+                    DrawingGallowBase();
+                    DrawingTopBaseAndRope();
+                    DrawingHead();
+                    DrawingBody(13);
+                    break;
+                }
+                case 5:
+                {
+                    DrawingGallowBase();
+                    DrawingTopBaseAndRope();
+                    DrawingHead();
+                    DrawingBody(13);
+                    DrawingLeftLeg(20);
+                    break;
+                }
+                case 6:
+                {
+                    DrawingGallowBase();
+                    DrawingTopBaseAndRope();
+                    DrawingHead();
+                    DrawingBody(13);
+                    DrawingLeftLeg(20);
+                    DrawingRightLeg(20);
+                    break;
+                }
+                case 7:
+                {
+                    DrawingGallowBase();
+                    DrawingTopBaseAndRope();
+                    DrawingHead();
+                    DrawingBody(13);
+                    DrawingLeftLeg(20);
+                    DrawingRightLeg(20);
+                    DrawingLeftArm(16);
+                    break;
+                }
+                case 8:
+                {
+                    DrawingGallowBase();
+                    DrawingTopBaseAndRope();
+                    DrawingHead();
+                    DrawingBody(13);
+                    DrawingLeftLeg(20);
+                    DrawingRightLeg(20);
+                    DrawingLeftArm(16);
+                    DrawingRightArmAndMessage(16);
+                    break;
+                }
+                case 9:
+                {
+                    DrawingGallowBase();
+                    DrawingTopBaseAndRope(); 
+                    HangMan();
+                    break;
+                }
+            }
         }
     }
 }
+
+
